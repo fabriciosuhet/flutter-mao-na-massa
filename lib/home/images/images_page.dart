46a5d6c7da5c967bd2743be1ea74cff5518b52b1
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-07-15 12:19:29 -0300

    images em flutter

---
+++ lib/home/images/images_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ImagesPage extends StatelessWidget {
+  const ImagesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Images'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: 300,
+              height: 250,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/rato.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'Paisagem',
+                  style: TextStyle(backgroundColor: Colors.white.withOpacity(0.6), fontSize: 20,),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.network(
+                'https://epipoca.com.br/wp-content/uploads/2022/01/Nezuko-em-Demon-Slayer-2.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_maonamassa/home/home_page.dart';
+// import 'package:flutter_maonamassa/home/home_page.dart';
+import 'package:flutter_maonamassa/home/images/images_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      // home: const HomePage(),
+      home: const ImagesPage(),
     );
   }
 }
